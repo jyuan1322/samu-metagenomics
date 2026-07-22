ffce8a3ce20c0863b0aa5a0da49073d2a788b09e
Jie Yuan <jyuan@cs.columbia.edu>
2026-07-22 14:33:10 -0400

    Added humann script for running humann3.9 and metaphlan4.1.1 with vJun23 metaphlan database - the database vesrion has to be specified through a humann argument, otherwise metaphlan will attempt to download a newer one. This is the verion set recommended by the authors.

--- process_fastqs/6_humann_validation_demo_run.sh
+++ process_fastqs/6_humann_validation_demo_run.sh
@@ -25,3 +25,26 @@ humann \
     --nucleotide-database "$HUMANN_CHOCOPHLAN_DB" \
     --protein-database "$HUMANN_UNIREF_DB" \
     --threads 8
+
+
+
+
+
+
+# ===============
+# Run Humann 3.9 with Metaphlan 4.1.1
+# ===============
+# find $(python -c "import humann; print(humann.__path__[0])") -iname "demo*"
+# to grab data path
+#
+micromamba activate humann39_env
+
+demo_data="/PHShome_actual/j/jy1008/.local/share/mamba/envs/humann39_env/lib/python3.11/site-packages/humann/tests/data/demo.fastq"
+HUMANN_CHOCOPHLAN_DB="/data/bwh-comppath-full/databases/humann/chocophlan/chocophlan"
+HUMANN_UNIREF_DB="/data/bwh-comppath-full/databases/humann/uniref/uniref"
+humann --input "$demo_data" \
+    --output demo_test_vJun23 \
+    --threads 4 \
+    --nucleotide-database "$HUMANN_CHOCOPHLAN_DB" \
+    --protein-database "$HUMANN_UNIREF_DB" \
+    --metaphlan-options "--bowtie2db /data/bwh-comppath-seq/databases/bowtie2 --index mpa_vJun23_CHOCOPhlAnSGB_202403"
